6678fb5b7d09a31a0ceb91d890ec262415e52c18
shamelj <70355802+shamelj@users.noreply.github.com>
2022-07-20 15:44:31 +0300

    Added inner class Node

--- Stack.cs
+++ Stack.cs
@@ -1,5 +1,11 @@
 ﻿namespace Collections.Generic;
-public class Stack
+public class Stack<T>
 {
-
+    class Node
+    {
+        Node? Next { get; set; } 
+        T Value { get; set; }
+        Node(T value) => Value = value;
+        Node(Node? next, T value) : this(value) => Next = next;
+    }
 }
