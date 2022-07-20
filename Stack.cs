1cb3f19fa50d8f1f39390032bcec6e23d3f89b6a
shamelj <70355802+shamelj@users.noreply.github.com>
2022-07-20 16:04:50 +0300

    Added head node for stack

--- Stack.cs
+++ Stack.cs
@@ -1,9 +1,10 @@
 ﻿namespace Collections.Generic;
 public class Stack<T>
 {
+    private Node? Head { get; set; }
     class Node
     {
-        Node? Next { get; set; } 
+        Node? Next { get; set; }
         T Value { get; set; }
         Node(T value) => Value = value;
         Node(Node? next, T value) : this(value) => Next = next;
